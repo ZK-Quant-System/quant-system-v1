26e34f307f23cceed3ba2ab8dd8918995631d0e9
Rick <rickzhou@RacheldeMacBook-Air.local>
2021-11-04 20:45:52 +0800

    adding reviews

--- Market_data/Market_data_service_automation.py
+++ Market_data/Market_data_service_automation.py
@@ -6,26 +6,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def cldata(df):
+##TODO 面向对象
+
+def cldata(df): ##TODO 函数名全称
     # 输人一个dataframe
     col = list(df.columns.values)#得到其列标签
     index = list(df.index.values)
     # 处理inf
-    df.replace(np.inf,np.nan,inplace=True)
+    df.replace(np.inf,np.nan,inplace=True)  ##TODO -np.inf
     # 处理不是nan的缺失值 
     # 处理非法字符，即非浮点数的字符串类。
+    ##TODO 这种遍历方式非常的不 pythonic 尽量多以向量化 apply等方式进行
     for i in index:
         for j in col:
-            if isinstance(df.loc[i,j],float):
+            if isinstance(df.loc[i,j],float):##TODO 逻辑有点问题，如果某个因子是类似于0，1这样的信号，也会直接pass
                 continue
             else:
                 df.loc[i,j]=np.nan
     return df
     
     
-    
+ ##TODO 加上主函数入口 编写主函数 以免多进程&线程时出现问题
 # 账号是申请时所填写的手机号；密码为聚宽官网登录密码，新申请用户默认为手机号后6位
-auth('13123205042','205042')
+auth('13123205042','205042')  ##TODO config
 
 # 获取所有中证指数股票, 设为股票池
 stocks1 = get_index_stocks('000300.XSHG')#沪深300
@@ -42,6 +45,7 @@ dfs = {stocks[i]:get_price(security=stocks[i], start_date=start_date, end_date=e
 date = list(dfs[stocks[0]].index.values)
 col = list(dfs[stocks[0]].columns.values)
 
+##TODO python尽量减少for循环 用map
 # 清洗数据
 for i in range(len(stocks)):
     df = dfs[stocks[0]]
