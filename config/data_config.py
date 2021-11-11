f3573ad20b816db84d3451be6835a3480798f950
xixi <92625768+huangyuqinxx@users.noreply.github.com>
2021-11-11 23:46:57 +0800

    Modify the data cleaning part, improve the running speed, and put frequency into config
    Modify the data cleaning part, improve the running speed, and put frequency into config

--- config/data_config.py
+++ config/data_config.py
@@ -1,3 +1,6 @@
-stock_index_list = ['000300.XSHG', '000905.XSHG', '000852.XSHG']
+from typing import List
+
+stock_index_list: List[str] = ['000300.XSHG', '000905.XSHG', '000852.XSHG']
 legal_type_list = [float, int]
 time_span = 2 * 365 * 24 * 60 * 60
+frequency='daily'
--- data_provider/market_data.py
+++ data_provider/market_data.py
@@ -3,14 +3,17 @@ from jqdatasdk import auth, get_index_stocks, get_price
 import time
 import numpy as np
 import click
+import sys
+sys.path.append('../')
 from config import data_config
+from data_deploy import TradingDates
 import json
 import pandas as pd
+import logging
 
 
 
 
-##TODO 面向对象
 class DataProvider:
     def __init__(self, data_provider_config):
         self.time_span = data_config.time_span
@@ -20,35 +23,39 @@ class DataProvider:
         self.jq_password = data_provider_config['jq_password']
         self.legal_type_list = data_config.legal_type_list
         self.stock_index_list = data_config.stock_index_list
+        self.frequency = data_config.frequency
         self.target_stocks_list = []
         auth(self.jq_account, self.jq_password)
 
-    def _clean_data(self, df):
-        # 处理inf
-        df.replace([np.inf, -np.inf], np.nan, inplace=True)
-        # 处理不是nan的缺失值
-        # 处理非法字符，即非浮点数的字符串类。
-        df_notlegal_type_list_locat = df.applymap(lambda x: False if type(x) in self.legal_type_list else True)
-        df[df_notlegal_type_list_locat] = np.nan
-        return df
 
+    # 处理非法字符，即非浮点整型数的字符串类，及inf。
+    def _replace2nan(self, x):
+        if type(x) in self.legal_type_list:
+            if x in [np.inf, -np.inf]:
+                return np.nan
+            else:
+                return x
+        else:
+            return np.nan
+
+
+    # 得到数据
     def get_data(self):
         for stock_index_name in self.stock_index_list:
             stock_index_data = get_index_stocks(stock_index_name)
             self.target_stocks_list.append(stock_index_data)
 
-        # dfs的关键词是股票代码，其对应值为两年的开盘、收盘等的dataframe
+        # 字典dfs的关键词是股票代码，其对应值为两年的开盘、收盘等的dataframe
         dfs = {self.target_stocks_list[i]: get_price(security=self.target_stocks_list[i], start_date=self.start_date,
-                                                     end_date=self.end_date, frequency='daily')
+                                                     end_date=self.end_date, frequency=self.frequency)
                for i in range(len(self.target_stocks_list))}
         col = list(dfs[self.target_stocks_list[0]].columns.values)
         
-        ##TODO python尽量减少for循环 用map
+
         # 清洗数据
-        for stock in self.target_stocks_list:
-            df = dfs[stock]
-            temp = self._clean_data(df)
-            dfs[stock] = temp
+        for (key, value) in dfs.items():
+            dfs[key] = value.applymap(_replace2nan)
+
         
         
         # 设置双索引 
@@ -64,8 +71,8 @@ class DataProvider:
         dfs_double_index = dfs_double_index.sort_values.sort_values(by=['date','code'],ascending=[True,True])
         dfs_double_index = dfs_double_index.set_index(['date','code'])
         # 存为csv和pkl格式
-        dfs_double_index.to_csv("market data with double index.csv")
-        dfs_double_index.to_pickle("market data with double index.pkl")
+        dfs_double_index.to_csv("./market_data_with_double_index/market_data_with_double_index.csv")
+        dfs_double_index.to_pickle("./market_data_with_double_index/market_data_with_double_index.pkl")
 
 
 
@@ -75,15 +82,18 @@ class DataProvider:
         pass
 
 
-##TODO 加上主函数入口 编写主函数 以免多进程&线程时出现问题
+
 @click.command()
 @click.option("--config_file", help="the config file for DataProvider", default="../config/config.json")
 def main(config_file):
     with open(config_file) as f:
         data_provider_config = json.load(f)
     data_provider = DataProvider(data_provider_config)
+    data_provider.get_data()
     data_provider.data_to_mongo()
 
 
 if __name__ == "__main__":
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+    logging.basicConfig(filename='my.log', level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
     main()
