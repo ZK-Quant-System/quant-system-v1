89bdf7714571a7b756b1a88b29dee2cdcb4e2253
xixi <92625768+huangyuqinxx@users.noreply.github.com>
2021-12-10 22:55:52 +0800

    The object-oriented of the function is modified, and the function is optimized and debugged.
    The object-oriented of the function is modified, and the function is optimized and debugged.

--- config/characteristic_config.py
+++ config/characteristic_config.py
@@ -1,12 +1,13 @@
-import pandas as pd
 from typing import List
 
 filling_methods: List[str] = ['ffill', 'rolling_mean', 'KNN']
-methon = ['roling_mean']
+instrument = 'instrument'
+datetime = 'datetime'
+method = 'rolling_mean'
 rolling_span = 3 # 大等于2
 KNN_k = 3
 max_value = 100000
 min_value = 0
 null_scale = 0.8
 trading_dates_file = '../data_deploy/TradingDates.csv'
-factor_file = '../data_deploy/TradingDates.csv'
+feature_file = '../factor_alpha/factor_alpha_1.csv'
