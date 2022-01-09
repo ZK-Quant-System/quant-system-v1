b82a9f766275315a8e17fee800fe1f9f76292fc5
xixi <92625768+huangyuqinxx@users.noreply.github.com>
2022-01-09 18:39:56 +0800

    Chang the path.
    Chang the path.

--- config/conbond_config.py
+++ config/conbond_config.py
@@ -2,11 +2,8 @@ legal_type_list = [float, int]
 list_status = '正常上市'
 time_span = 2 * 365 * 24 * 60 * 60
 fields = ['date', 'code', 'open', 'close', 'high', 'low', 'volume', 'money']
-#data_path='/ZK-quant-system/data/'
+data_path='/ZK-quant-system/data/'
 conbond_market_data_file = data_path + "conbond_market_data_with_double_index.pkl"
-#dates_path='/ZK-quant-system/gitrepos/quant-system-v1/data_deploy/'
-#trading_dates_file = dates_path+'TradingDates.csv'
-
-data_path='G:/Workfiles/ZK-Quant-System/data/'
-dates_path='G:/Workfiles/ZK-Quant-System/GitHub/data_deploy/'
+dates_path='/ZK-quant-system/gitrepos/quant-system-v1/data_deploy/'
 trading_dates_file = dates_path+'TradingDates.csv'
+
