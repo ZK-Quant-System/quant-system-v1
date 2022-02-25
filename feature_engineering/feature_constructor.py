593fef5695e6d0b972e753129e7f1cf05b113588
chloewang087 <1073683498@qq.com>
2022-02-25 18:15:14 +0800

    add a pipeline for feature engineering

--- feature_engineering/feature_constructor.py
+++ feature_engineering/feature_constructor.py
@@ -13,142 +13,95 @@ from gplearn.genetic import SymbolicTransformer
 from config import feature_config
 
 
+def label_ret(df:pd.DataFrame, n):
+    df_label = df.pct_change(periods=n)
+    return df_label
+
+
 def standardized(df_feature: pd.DataFrame):
-    array_feature = np.array(df_feature).reshape(-1, 1)
-    df_new = pd.DataFrame(StandardScaler().fit_transform(array_feature))
-    df_new.columns = ['Standardized_' + df_feature.columns.values]
+    df_new = pd.DataFrame(StandardScaler().fit_transform(df_feature))
+    df_new.columns = 'Standardized_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
 
 
 def minmax(df_feature: pd.DataFrame):
-    array_feature = np.array(df_feature).reshape(-1, 1)
-    df_new = pd.DataFrame(MinMaxScaler().fit_transform(array_feature))
-    df_new.columns = ['MinMax_' + df_feature.columns.values]
+    df_new = pd.DataFrame(MinMaxScaler().fit_transform(df_feature))
+    df_new.columns = 'MinMax_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
 
 
 def maxabs(df_feature: pd.DataFrame):
-    array_feature = np.array(df_feature).reshape(-1, 1)
-    df_new = pd.DataFrame(MaxAbsScaler().fit_transform(array_feature))
-    df_new.columns = ['MaxAbs_' + df_feature.columns.values]
+    df_new = pd.DataFrame(MaxAbsScaler().fit_transform(df_feature))
+    df_new.columns = 'MaxAbs_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
 
 
 def normalization(df_feature):
-    array_feature = np.array(df_feature).reshape(-1, 1)
-    df_new = pd.DataFrame(Normalizer().fit_transform(array_feature))
-    df_new.columns = ['Norm_' + df_feature.columns.values]
+    df_new = pd.DataFrame(Normalizer().fit_transform(df_feature))
+    df_new.columns = 'Norm_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
 
 
 def isometry_cut(df_feature: pd.DataFrame, num):
     df_new = pd.DataFrame(pd.cut(df_feature.stack(), num))
-    df_new.columns = ['Isocut_' + str(num)]
+    df_new.columns = 'IsometryCut_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
 
 
 def equifreq_cut(df_feature: pd.DataFrame, num):
     df_new = pd.DataFrame(pd.qcut(df_feature.stack(), num))
-    df_new.columns = ['Equifreqcut_' + str(num)]
-    df_new.index = df_feature.index
-    return df_new
-
-
-def kmeans(df_feature: pd.DataFrame, feature, k):
-    """
-    基于k均值聚类的分箱：k均值聚类法将观测值聚为k类
-    :param df_feature: 包括所有特征因子的dataframe
-    :param feature: 须进行聚类分箱的因子列名
-    :param k: 聚类数量
-    :return: df_new: 对指定列的分箱结果
-    """
-    length = len(df_feature)
-    kmodel = KMeans(n_clusters=k)  # 聚成几类
-    kmodel.fit(df_feature.values.reshape(length, -1))  # 训练模型
-    center = pd.DataFrame(kmodel.cluster_centers_, columns=[feature])  # 求聚类中心
-    center = center.sort_values(by=feature)  # 排序
-    x = center.rolling(2).mean().iloc[1:, ]  # 滑窗计算均值
-    w = [df_feature[feature].min() - 1] + list(x.loc[:, feature]) + [df_feature[feature].max()]  # 划分依据 (a,b]形式
-    df_new = pd.DataFrame(pd.cut(df_feature[feature], w))  # 划分
-    df_new.columns = ['Kmeans_' + feature]
+    df_new.columns = 'EquifreqCut_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
 
 
 def binarizer(df_feature: pd.DataFrame, thre: float = None):
-    array_feature = np.array(df_feature).reshape(-1, 1)
-    df_new = pd.DataFrame(Binarizer(threshold=thre).fit_transform(array_feature))
-    df_new.columns = ['binarizer' + df_feature.columns]
+    df_new = pd.DataFrame(Binarizer(threshold=thre).fit_transform(df_feature))
+    df_new.columns = 'binarizer_' + df_feature.columns
     df_new.index = df_feature.index
     return df_new
-
-
-def BoxCox(df_feature: pd.DataFrame):
-    """
-    Box-Cox变换：须输入正数
-    """
-    a_feature = np.array(df_feature)
-    feature_clean = a_feature[~np.isnan(a_feature)]  # 剔除零值
-    l, opt_lambda = stats.boxcox(feature_clean)  # 计算最佳lamba
-    df_new = pd.DataFrame(stats.boxcox(feature_clean.flatten(), lmbda=opt_lambda))
-    df_new.columns = ['boxcox' + df_feature.columns]
-    df_new.index = df_feature.index
-    return df_new
-
-
-def labelencoder(df_feature):
-    feature_array = np.array(df_feature.stack())
-    feature_list = feature_array.tolist()
-    le = LabelEncoder()
-    le.fit(feature_list)
-    df_new = pd.DataFrame(le.transform(feature_list))
-    df_new.columns = ['LabelEncoder']
-    return df_new
-
-
-def onehotencoder(factor):
-    pass
 
 
 def feature_log(df_feature: pd.DataFrame):
     data = pd.DataFrame(np.log(df_feature))
-    data.columns = ['log_' + df_feature.columns]
+    data.columns = 'log_' + df_feature.columns
     data.index = df_feature.index
     return data
 
 
 def feature_power(df_feature: pd.DataFrame, power: int = None):
     data = pd.DataFrame(df_feature ** power)
-    data.columns = ['power_ ' + str(power)]
+    data.columns = df_feature.columns + 'power_' + str(power)
     data.index = df_feature.index
     return data
 
 
 def feature_sqrt(df_feature: pd.DataFrame):
     data = pd.DataFrame(np.sqrt(df_feature))
-    data.columns = ['sqrt_' + df_feature.columns]
+    data.columns = 'sqrt_' + df_feature.columns
     data.index = df_feature.index
     return data
 
 
 def feature_exp(df_feature: pd.DataFrame):
     data = pd.DataFrame(np.exp(df_feature))
-    data.columns = ['exp_' + df_feature.columns]
+    data.columns = 'exp_' + df_feature.columns
     data.index = df_feature.index
     return data
 
 
 def feature_diff(df_feature: pd.DataFrame):
     data = df_feature.diff()
-    data.columns = ['diff']
+    data.columns = 'diff_'+ df_feature.columns
     data.index = df_feature.index
     return data
 
+"""以下待修改"""
 
 def cartesian(feature_df, feature1_name, feature2_name):
     feature1_df = pd.get_dummies(feature_df[feature1_name])
@@ -191,4 +144,49 @@ def symbolictransformer(features: pd.DataFrame, target: pd.DataFrame, prop: floa
     return features_df
 
 
+def labelencoder(df_feature):
+    feature_array = np.array(df_feature.stack())
+    feature_list = feature_array.tolist()
+    le = LabelEncoder()
+    le.fit(feature_list)
+    df_new = pd.DataFrame(le.transform(feature_list))
+    df_new.columns = ['LabelEncoder']
+    return df_new
 
+
+def onehotencoder(factor):
+    pass
+
+
+def kmeans(df_feature: pd.DataFrame, feature, k):
+    """
+    基于k均值聚类的分箱：k均值聚类法将观测值聚为k类
+    :param df_feature: 包括所有特征因子的dataframe
+    :param feature: 须进行聚类分箱的因子列名
+    :param k: 聚类数量
+    :return: df_new: 对指定列的分箱结果
+    """
+    length = len(df_feature)
+    kmodel = KMeans(n_clusters=k)  # 聚成几类
+    kmodel.fit(df_feature.values.reshape(length, -1))  # 训练模型
+    center = pd.DataFrame(kmodel.cluster_centers_, columns=[feature])  # 求聚类中心
+    center = center.sort_values(by=feature)  # 排序
+    x = center.rolling(2).mean().iloc[1:, ]  # 滑窗计算均值
+    w = [df_feature[feature].min() - 1] + list(x.loc[:, feature]) + [df_feature[feature].max()]  # 划分依据 (a,b]形式
+    df_new = pd.DataFrame(pd.cut(df_feature[feature], w))  # 划分
+    df_new.columns = ['Kmeans_' + feature]
+    df_new.index = df_feature.index
+    return df_new
+
+
+def BoxCox(df_feature: pd.DataFrame):
+    """
+    Box-Cox变换：须输入正数
+    """
+    a_feature = np.array(df_feature)
+    feature_clean = a_feature[~np.isnan(a_feature)]  # 剔除零值
+    l, opt_lambda = stats.boxcox(feature_clean)  # 计算最佳lamba
+    df_new = pd.DataFrame(stats.boxcox(feature_clean.flatten(), lmbda=opt_lambda))
+    df_new.columns = ['boxcox' + df_feature.columns]
+    df_new.index = df_feature.index
+    return df_new
