35919fdd8efeb750e2c3bd4ee556b661762293ca
chloewang087 <1073683498@qq.com>
2022-04-11 20:37:50 +0800

    amend feature_constructor

--- feature_engineering/feature_constructor.py
+++ feature_engineering/feature_constructor.py
@@ -101,7 +101,63 @@ def feature_diff(df_feature: pd.DataFrame):
     data.index = df_feature.index
     return data
 
-"""以下待修改"""
+
+def symbolictransformer(features: pd.DataFrame, target: pd.DataFrame, prop: float = None):
+    """
+    基于gplearn的符号转化器
+    :param features: 输入特征值
+    :param target: 目标预测值
+    :param prop: 训练集的比例
+    :return: features_df: 生成新特征
+    """
+    features = np.array(features)
+    target = np.array(target)
+    gp = SymbolicTransformer(generations=10, population_size=1000,
+                             hall_of_fame=100, n_components=10,
+                             function_set=feature_config.function_set,
+                             parsimony_coefficient=0.0005,
+                             max_samples=0.9, verbose=1,
+                             random_state=0, n_jobs=3)
+    num = int(len(features) * prop)
+    gp.fit(features[:num, :], target[:num])
+    gp_features = gp.transform(features)
+    features_df = pd.DataFrame(gp_features)
+    features_df.columns = ['SymbolicTransformer_' + str(i) for i in range(features_df.shape[0]) ]
+    return features_df
+
+def kmeans(df_feature: pd.DataFrame, feature, k):
+    """
+    基于k均值聚类的分箱：k均值聚类法将观测值聚为k类
+    :param df_feature: 包括所有特征因子的dataframe
+    :param feature: 须进行聚类分箱的因子列名
+    :param k: 聚类数量
+    :return: df_new: 对指定列的分箱结果
+    """
+    length = len(df_feature)
+    kmodel = KMeans(n_clusters=k)  # 聚成几类
+    kmodel.fit(df_feature.values.reshape(length, -1))  # 训练模型
+    center = pd.DataFrame(kmodel.cluster_centers_, columns=[feature])  # 求聚类中心
+    center = center.sort_values(by=feature)  # 排序
+    x = center.rolling(2).mean().iloc[1:, ]  # 滑窗计算均值
+    w = [df_feature[feature].min() - 1] + list(x.loc[:, feature]) + [df_feature[feature].max()]  # 划分依据 (a,b]形式
+    df_new = pd.DataFrame(pd.cut(df_feature[feature], w))  # 划分
+    df_new.columns = ['kmeans_' + feature]
+    df_new.index = df_feature.index
+    return df_new
+
+
+def BoxCox(df_feature: pd.DataFrame):
+    """
+    Box-Cox变换：须输入正数
+    """
+    a_feature = np.array(df_feature)
+    feature_clean = a_feature[~np.isnan(a_feature)]  # 剔除零值
+    l, opt_lambda = stats.boxcox(feature_clean)  # 计算最佳lamba
+    df_new = pd.DataFrame(stats.boxcox(feature_clean.flatten(), lmbda=opt_lambda))
+    df_new.columns = ['BoxCox_' + df_feature.columns]
+    df_new.index = df_feature.index
+    return df_new
+
 
 def cartesian(feature_df, feature1_name, feature2_name):
     feature1_df = pd.get_dummies(feature_df[feature1_name])
@@ -121,29 +177,6 @@ def cartesian(feature_df, feature1_name, feature2_name):
     return combine_df
 
 
-def symbolictransformer(features: pd.DataFrame, target: pd.DataFrame, prop: float = None):
-    """
-    基于gplearn的符号转化器
-    :param features:
-    :param target:
-    :param prop: 训练集的比例
-    :return: features_df: 生成新特征
-    """
-    features = np.array(features)
-    target = np.array(target)
-    gp = SymbolicTransformer(generations=10, population_size=1000,
-                             hall_of_fame=100, n_components=10,
-                             function_set=feature_config.function_set,
-                             parsimony_coefficient=0.0005,
-                             max_samples=0.9, verbose=1,
-                             random_state=0, n_jobs=3)
-    num = int(len(features) * prop)
-    gp.fit(features[:num, :], target[:num])
-    gp_features = gp.transform(features)
-    features_df = pd.DataFrame(gp_features)
-    return features_df
-
-
 def labelencoder(df_feature):
     feature_array = np.array(df_feature.stack())
     feature_list = feature_array.tolist()
@@ -156,37 +189,3 @@ def labelencoder(df_feature):
 
 def onehotencoder(factor):
     pass
-
-
-def kmeans(df_feature: pd.DataFrame, feature, k):
-    """
-    基于k均值聚类的分箱：k均值聚类法将观测值聚为k类
-    :param df_feature: 包括所有特征因子的dataframe
-    :param feature: 须进行聚类分箱的因子列名
-    :param k: 聚类数量
-    :return: df_new: 对指定列的分箱结果
-    """
-    length = len(df_feature)
-    kmodel = KMeans(n_clusters=k)  # 聚成几类
-    kmodel.fit(df_feature.values.reshape(length, -1))  # 训练模型
-    center = pd.DataFrame(kmodel.cluster_centers_, columns=[feature])  # 求聚类中心
-    center = center.sort_values(by=feature)  # 排序
-    x = center.rolling(2).mean().iloc[1:, ]  # 滑窗计算均值
-    w = [df_feature[feature].min() - 1] + list(x.loc[:, feature]) + [df_feature[feature].max()]  # 划分依据 (a,b]形式
-    df_new = pd.DataFrame(pd.cut(df_feature[feature], w))  # 划分
-    df_new.columns = ['Kmeans_' + feature]
-    df_new.index = df_feature.index
-    return df_new
-
-
-def BoxCox(df_feature: pd.DataFrame):
-    """
-    Box-Cox变换：须输入正数
-    """
-    a_feature = np.array(df_feature)
-    feature_clean = a_feature[~np.isnan(a_feature)]  # 剔除零值
-    l, opt_lambda = stats.boxcox(feature_clean)  # 计算最佳lamba
-    df_new = pd.DataFrame(stats.boxcox(feature_clean.flatten(), lmbda=opt_lambda))
-    df_new.columns = ['boxcox' + df_feature.columns]
-    df_new.index = df_feature.index
-    return df_new
